4b77f368c08cce1a844ef4e78ebbacc7d75fa9a1
kyleyannelli <kyleyannelli@gmail.com>
2024-12-21 20:02:16 -0500

    nvim: faster smear cursor

--- nvim/.config/nvim/lua/kmfg/lazy.lua
+++ nvim/.config/nvim/lua/kmfg/lazy.lua
@@ -28,6 +28,10 @@ require("lazy").setup({
             -- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
             -- Smears will blend better on all backgrounds.
             legacy_computing_symbols_support = false,
+            stiffness = 0.8,
+            trailing_stiffness = 0.5,
+            distance_stop_animating = 0.5,
+            hide_target_hack = false,
         },
     },
     {
